881b222d7268c86138fe0c2ef87eda646818f79b
Kevin John Jubahib <jubahibkevinjohn@gmail.com>
2019-04-03 23:39:49 +0800

    Update Insert Meals

--- api/insert-meals.php
+++ api/insert-meals.php
@@ -4,12 +4,33 @@ include 'functions.php';
 
 $json_response = array(); 
 
+//function
+function getCredits($mysqli,$id){
+	$credit = 0;
+	$query = mysqli_query($mysqli,"SELECT credit FROM tbl_food_variety WHERE id = '$id'");
+	if (mysqli_num_rows($query) > 0) {			
+				
+		 while($row = mysqli_fetch_assoc($query)) {
+		 	
+		 	$credit = $row['credit'];		 				 	
+		 }			 		
+			
+	}else{
+		$row_array['message'] = "ID is out of the list";
+		array_push($json_response,$row_array); 
+		 exit;
+	}
+	return $credit;	
+}
 
-if(isset($_GET['student_id']) && isset($_GET['variety']) && isset($_GET['servings']) && isset($_GET['credits']) && isset($_GET['date_eaten'])){
+// if(isset($_GET['student_id']) && isset($_GET['variety']) && isset($_GET['servings']) && isset($_GET['credits']) && isset($_GET['date_eaten'])){
+if(isset($_GET['student_id']) && isset($_GET['variety']) && isset($_GET['servings'])  && isset($_GET['date_eaten'])){
+
 	$student_id = $_GET['student_id'];
 	$variety =	$_GET['variety'];
 	$servings = $_GET['servings'];
-	$credits  = $_GET['credits'];
+	$credits  = getCredits($mysqli,$variety) * $servings;
+	
 	$date_eaten = $_GET['date_eaten'];
 
 	$sql = "INSERT INTO tbl_meals (								
